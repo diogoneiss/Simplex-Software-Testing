85eb2939dc0443641bc7ffde0f2108f4a0dc6bba
diogo neiss <diogo.oliveiran@gmail.com>
2022-09-29 10:14:00 -0300

    Arrumado import de modulos para testes

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,22 @@ import os
 from pathlib import Path
 
 import sys
-from pathlib import Path
-print("\nEntering conftest.py (first file before tests run) ")
-PROJECT_DIR = Path(__file__).resolve().parents[1] / "src" / "Utils"
-print("Project dir: ", PROJECT_DIR)
-#sys.path.append( str(PROJECT_DIR))
-print("sys.path: ")
-print(sys.path)
-print("____")
-from Utils.read_json import inject_test_data
+ADD_TO_SYSPATH = True
 
+# I dont know if this is needed
+# tried and had no effect
+if ADD_TO_SYSPATH:
+
+    print("\nEntering conftest.py (first file before tests run) ")
+    PROJECT_DIR = Path(__file__).resolve().parents[1] / "src" 
+    print("Project dir: ", PROJECT_DIR)
+
+    sys.path.append( str(PROJECT_DIR))
+    print("sys.path: ")
+    print(sys.path)
+    print("____")
+
+from src.Utils.read_json import inject_test_data
 
 def pytest_configure():
     pytest.input_test_data = get_json_input()
